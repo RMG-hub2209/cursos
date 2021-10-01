7998ea8e306e0e788206186c184d671ff8cf2a8a
surftware-RMG <rafael.madrigal@ciencias.unam.mx>
2021-10-01 15:28:50 -0500

    resueltolibros

--- ejercicioTiendalibros.py
+++ ejercicioTiendalibros.py
@@ -1,4 +1,18 @@
 tituloLibro = input('Ingrese el Título del libro: ')
 idLibro = int(input('Ingrese el Id del libro: '))
 precioLibro = float(input('Ingrese el precio del libro: '))
-envioLibro = bool(input('¿El envio fue gratis?: (True/False)'))
+envioLibro = input('¿El envio fue gratis? (True/False): ')
+
+if envioLibro == 'True':
+    envioLibro = True
+elif envioLibro == 'False':
+    envioLibro = False
+else:
+    envioLibro = input('Dato incorrecto, debe indicar si es True/False: ')
+
+print (f'''
+   Titulo: {tituloLibro}
+   ID: {idLibro}
+   Precio: {precioLibro}
+   Envio: {envioLibro}
+''')
